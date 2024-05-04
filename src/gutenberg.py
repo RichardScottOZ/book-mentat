0e8248c709b9c8fe5ef56fee17ba3ee0ee3de979
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-04 18:24:57 +0930

    austen

--- src/gutenberg.py
+++ src/gutenberg.py
@@ -5,7 +5,8 @@ embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 from llama_index.core import SimpleDirectoryReader
 
 loader = SimpleDirectoryReader(
-    input_dir=".test/library/jane-austen/pride-and-prejudice.epub",
+    #input_dir=".test/library/jane-austen/pride-and-prejudice.epub",
+    input_dir=".test/library/jane-austen",
     #required_exts=[".epub"],
     recursive=False,
 
