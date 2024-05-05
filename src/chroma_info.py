a8308aae09078d40b179a083e0528fcba6bb9b53
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-05 18:07:03 +0930

    collection_name

--- src/chroma_info.py
+++ src/chroma_info.py
@@ -9,14 +9,14 @@ from llama_index.llms.ollama import Ollama
 embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 chroma_path = "/mnt/usb_mount/chroma/Calibre Books"
-collection = "calibrebooks"
+collection_name = "calibrebooks"
 
 print("LOAD CHROMA INDEX CHECK")
 
 # load from disk
 db2 = chromadb.PersistentClient(path=chroma_path)
 #client = chromadb.PersistentClient(path="/path/to/save/to")
-collection = db2.get_collection(name="my_collection", embedding_function=embedding_model)
+collection = db2.get_collection(name=collection_name, embedding_function=embedding_model)
 
 print(collection.count())
 if 1 == 2:
