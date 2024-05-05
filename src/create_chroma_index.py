22aec59fc9f6de73546825b0aefbd263485bc5e2
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-05 10:56:04 +0930

    books path

--- src/create_chroma_index.py
+++ src/create_chroma_index.py
@@ -10,7 +10,7 @@ embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 from llama_index.core import SimpleDirectoryReader
 
 input_path = "/mnt/usb_mount/output/Calibre Books"
-input_path="test"
+#input_path="test"
 
 print("LOADING CALIBRE BOOKS in epub")
 
