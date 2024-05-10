c8f1b990076df4c1448e19a90e6a4b36d812f7bd
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-10 19:48:01 +0930

    batch start

---
+++ src/chroma_info_custom_batch.py
@@ -0,0 +1,48 @@
+import sys
+
+model = sys.argv[1]
+strq = sys.argv[2]
+print("mode:",model,"query:",strq)
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+#from IPython.display import Markdown, display
+import chromadb
+from llama_index.llms.ollama import Ollama
+
+embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+chroma_path = "/mnt/usb_mount/chroma/Calibre Books"
+collection_name = "calibrebooks"
+
+# note on custom embed class to get around signature error
+# https://stackoverflow.com/questions/77555461/facing-issue-while-running-chroma-from-documents-function
+
+from chromadb.utils import embedding_functions
+from chromadb import Documents, EmbeddingFunction, Embeddings
+class MyEmbeddingFunction(EmbeddingFunction[Documents]):
+    def __call__(self, input: Documents) -> Embeddings:
+        sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="BAAI/bge-small-en-v1.5")
+        embeddings = sentence_transformer_ef(input)
+        return embeddings
+
+custom = MyEmbeddingFunction()
+
+
+print("LOAD CHROMA INDEX CHECK")
+
+# load from disk
+db2 = chromadb.PersistentClient(path=chroma_path)
+#client = chromadb.PersistentClient(path="/path/to/save/to")
+#collection = db2.get_collection(name=collection_name, embedding_function=embedding_model)
+collection = db2.get_collection(name=collection_name, embedding_function=custom)
+
+print(collection.count())
+
+
+existing_count = col.count()
+batch_size = 10
+for i in range(0, existing_count, batch_size):
+    batch = col.get(include=["metadatas", "documents", "embeddings"], limit=batch_size, offset=i)
+
