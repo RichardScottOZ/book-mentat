56508f842e4fd2fd6e15e2ccd9cf808d51179f0c
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-11 09:50:02 +0930

    check books

---
+++ src/check_books_done.py
@@ -0,0 +1,15 @@
+import pickle
+with open('book_dict.pkl','rb') as f:
+    book_dict = pickle.load(sorted_dict, f)
+
+input_path = "/mnt/usb_mount/output/Calibre Books"
+file_list = []
+for root, dirs, files in os.walk(input_path):
+    for file in files:
+        file_list.append(file)
+
+for b in file_list:
+    if b in book_dict:
+        pass
+    else:
+        print("STILL NEED:",b)
