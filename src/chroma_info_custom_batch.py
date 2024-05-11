4cc17661607de4a86026f25df97ee29023efaab5
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-11 12:52:23 +0930

    rerank utils

--- src/chroma_info_custom_batch.py
+++ src/chroma_info_custom_batch.py
@@ -11,6 +11,11 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 import chromadb
 from llama_index.llms.ollama import Ollama
 
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core import QueryBundle
+import pandas as pd
+#from IPython.display import display, HTML
+
 embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 chroma_path = "/mnt/usb_mount/chroma/Calibre Books"
@@ -28,6 +33,42 @@ class MyEmbeddingFunction(EmbeddingFunction[Documents]):
         return embeddings
 
 custom = MyEmbeddingFunction()
+
+def get_retrieved_nodes(
+    query_str, vector_top_k=10, reranker_top_n=3, with_reranker=False
+):
+    query_bundle = QueryBundle(query_str)
+    # configure retriever
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=vector_top_k,
+    )
+    retrieved_nodes = retriever.retrieve(query_bundle)
+
+    if with_reranker:
+        # configure reranker
+        reranker = LLMRerank(
+            choice_batch_size=5,
+            top_n=reranker_top_n,
+        )
+        retrieved_nodes = reranker.postprocess_nodes(
+            retrieved_nodes, query_bundle
+        )
+
+    return retrieved_nodes
+
+def pretty_print(df):
+    return display(HTML(df.to_html().replace("\\n", "<br>")))
+
+
+def visualize_retrieved_nodes(nodes) -> None:
+    result_dicts = []
+    for node in nodes:
+        result_dict = {"Score": node.score, "Text": node.node.get_text()}
+        result_dicts.append(result_dict)
+
+    #pretty_print(pd.DataFrame(result_dicts))
+    print(pd.DataFrame(result_dicts))
 
 
 print("LOAD CHROMA INDEX CHECK")
