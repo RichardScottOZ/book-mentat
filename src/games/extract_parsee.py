8ab086a325f8e28499f0583b20c201eb00de33f4
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-12 15:58:50 +0930

    dump the elements

--- src/games/extract_parsee.py
+++ src/games/extract_parsee.py
@@ -1,14 +1,22 @@
 import os
 from pdf_reader import get_elements_from_pdf
+import pickle
 
 input_path = "/mnt/usb_mount/books/Calibre Library"
+output_path = "/mnt/usb_mount/games/parsee"
+
+os.makedirs(output_path, exist_ok=True)
+
 for root, dirs, files in os.walk(input_path):
     for file in files:
         if '.pdf' in file:
             print(file)
+            newfile = file + .'pkl'
             elements = get_elements_from_pdf(os.path.join(root,file))
-            print(elements)
-            break
+            with open(os.path.join(output_path,file,'.pkl'),'wb') as f:
+                pickle.dump(elements, f)
+            #print(elements)
+            #break
 
     #break
 #If you are processing a PDF that needs OCR but no elements or just very few are being returned, you can force OCR like this (replace the paths):
