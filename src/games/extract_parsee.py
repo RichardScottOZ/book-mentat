976ccd3c5b8339f505830cf722cbb81f6d91aeb9
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-12 16:02:51 +0930

    parsee errors

--- src/games/extract_parsee.py
+++ src/games/extract_parsee.py
@@ -6,19 +6,30 @@ input_path = "/mnt/usb_mount/books/Calibre Library"
 output_path = "/mnt/usb_mount/games/parsee"
 
 os.makedirs(output_path, exist_ok=True)
+counterror = 0
 
 for root, dirs, files in os.walk(input_path):
     for file in files:
         if '.pdf' in file:
             print(file)
             newfile = file + .'pkl'
-            elements = get_elements_from_pdf(os.path.join(root,file))
-            with open(os.path.join(output_path,file,'.pkl'),'wb') as f:
-                pickle.dump(elements, f)
+            try:
+                elements = get_elements_from_pdf(os.path.join(root,file))
+                with open(os.path.join(output_path,newfile,'.pkl'),'wb') as f:
+                    pickle.dump(elements, f)
+            except Exception as parseE:
+                newfile = file + .'error'
+                print("ERROR:",file)
+                counterror += 1
+                with open(os.path.join(output_path,newfile),'wb') as f:
+                    pickle.dump(elements, f)
+
+                
             #print(elements)
             #break
 
     #break
+print("FINISHED: and there were ",counterror failures)    
 #If you are processing a PDF that needs OCR but no elements or just very few are being returned, you can force OCR like this (replace the paths):
 
 #elements = get_elements_from_pdf("FILE_PATH", force_ocr=True)
