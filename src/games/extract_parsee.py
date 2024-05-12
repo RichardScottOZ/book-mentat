ce26e3ef4ad9fa141d3263c08a32a193a65fda78
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-12 16:04:31 +0930

    failures

--- src/games/extract_parsee.py
+++ src/games/extract_parsee.py
@@ -30,7 +30,7 @@ for root, dirs, files in os.walk(input_path):
             #break
 
     #break
-print("FINISHED: and there were ",counterror failures)    
+print("FINISHED: and there were ",counterror, " failures")    
 #If you are processing a PDF that needs OCR but no elements or just very few are being returned, you can force OCR like this (replace the paths):
 
 #elements = get_elements_from_pdf("FILE_PATH", force_ocr=True)
