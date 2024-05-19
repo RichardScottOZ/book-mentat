dcae7e4977e21e458c4aec2bc70e2a406666c7e8
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-19 11:55:07 +0930

    counts

--- src/games/check_complete.py
+++ src/games/check_complete.py
@@ -5,18 +5,32 @@ output_path = "/mnt/usb_mount/games/parsee"
 
 counterror = 0
 
-count = -1
+count = 0
+count_complete = 0
+count_incomplete = 0
+count_problem = 0
 
 with open(os.path.join(input_path,'complete.log'),'w') as f:
     for root, dirs, files in os.walk(input_path):
         for file in files:
             if '.pdf' not in file or '.log' in file:
                 continue
+            count += 1
             if os.path.exists(os.path.join(output_path,file + '.pkl')):
                 print("COMPLETE:", os.path.join(root,file))
                 f.writelines("COMPLETE: " + os.path.join(root,file) + "\n")
+                count_complete += 1
             else:
                 print("NEED TO CHECK:", os.path.join(root,file))
                 f.writelines("NEED TO CHECK: " + os.path.join(root,file) + "\n")
+                count_incomplete += 1
+
+            if os.path.exists(os.path.join(output_path,file + '.error')):
+                print("ERROR:", os.path.join(root,file))
+                f.writelines("COMPLETE: " + os.path.join(root,file) + "\n")
+                count_problem += 1
+
+
+print("TOTAL PDF:",count, "COMPLETE:", count_complete, "NOT:", count_incomplete, "ERROR:", count_problem)                
 
 
