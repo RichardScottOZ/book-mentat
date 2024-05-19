e60f6280c259c98a28251a2c87038e88362f2ce3
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-19 11:20:07 +0930

    log

--- src/games/check_complete.py
+++ src/games/check_complete.py
@@ -9,9 +9,12 @@ count = -1
 
 for root, dirs, files in os.walk(input_path):
     for file in files:
+        if '.pdf' not in file or '.log' in file:
+            continue
         if os.path.exists(os.path.join(output_path,file + '.pkl')):
             print("COMPLETE:", file)
         else:
             print("NEED TO CHECK:", file)
+            break
 
 
