ee9f23fea9f8bcc64a9ff5bf4d1e517a9f15f3d6
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-24 21:44:27 +0930

    parseenumber

--- src/games/check_game.py
+++ src/games/check_game.py
@@ -4,9 +4,9 @@ from pdf_reader import get_elements_from_pdf
 import pickle
 
 input_path = "/mnt/usb_mount/books/Calibre Library"
-output_path = "/mnt/usb_mount/games/parsee"
+output_path = "/mnt/usb_mount/games/parseenumber"
 
-file = ''
+file = 'DC Heroes - Bruce Humphrey Rigged Results.pkl'
 with open(os.path.join(output_path,file)) as f:
     elements = pickle.load(f)
 
