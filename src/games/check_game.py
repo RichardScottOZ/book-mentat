ba34764eb5d1e748822157ba34efb47f382d9f17
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-24 22:11:33 +0930

    file 2

--- src/games/check_game.py
+++ src/games/check_game.py
@@ -7,6 +7,7 @@ input_path = "/mnt/usb_mount/books/Calibre Library"
 output_path = "/mnt/usb_mount/games/parseenumber"
 
 file = 'DC Heroes - Rigged Results - Bruce Humphrey.pdf_15225.pkl'
+file = 'D&D - d20 Pathfinder Village Ba - Ben Kent.pdf_22822.pkl'
 
 with open(os.path.join(output_path,file),'rb') as f:
     elements = pickle.load(f)
