c6c15211cf701b706eac4b64791a645a78cba203
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-26 09:03:15 +0930

    filepath

--- src/games/lithops_list.py
+++ src/games/lithops_list.py
@@ -10,9 +10,10 @@ from multiprocessing import Pool
 
 def process_pdf(filepath):
     for root, dirs, files in os.walk(filepath):
+        print(filepath)
         for file in files:
             print(file)
-            
+
 if __name__ == "__main__":
     input_path = "books/Calibre Library"
     output_path = "games/parseenumber"
