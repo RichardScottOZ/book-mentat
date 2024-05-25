e3d94d75593fd25969f70a864926a74d796c5a2f
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-26 09:15:36 +0930

    list cloud

--- src/games/lithops_list_cloud.py
+++ src/games/lithops_list_cloud.py
@@ -3,17 +3,16 @@ import os
 from lithops import FunctionExecutor
 from lithops.storage.cloud_proxy import os as cloudos, open as cloudopen
 from lithops import Storage
-Storage.list_objects()
+
 from pdf_reader import get_elements_from_pdf
 import pickle
 from multiprocessing import Pool
 
 def process_pdf(filepath):
-    for root, dirs, files in cloudos.walk(filepath):
-        print(filepath)
-        for file in files:
-            print(file)
-
+    spdf = Storage()
+    flist = Storage.list_objects()
+    for f in flist:
+        print(f)
 if __name__ == "__main__":
     input_path = "books/Calibre Library/"
     output_path = "games/parseenumber"
