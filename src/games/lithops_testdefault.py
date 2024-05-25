bf50637f723f509071674878c9e294239f4496df
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-25 18:48:33 +0930

    main

--- src/games/lithops_testdefault.py
+++ src/games/lithops_testdefault.py
@@ -11,21 +11,12 @@ def process_pdf(file):
     df['boring'] = [1]
     return print(df)
 
-if __name__ == "__main__":
-    input_path = "/mnt/usb_mount/books/Calibre Library"
-    output_path = "/mnt/usb_mount/games/parseenumber"
-    filetest = 'Anodyne Printware/Mothership - HULL BREACH VOL. 1 (25633)/Mothership - HULL BREACH VOL. 1 - Anodyne Printware.pdf'
-    #os.makedirs(output_path, exist_ok=True)
-    #counterror = 0
 
-    
+with FunctionExecutor(runtime='default-runtime') as fexec:
 
-
-    with FunctionExecutor(runtime='default-runtime') as fexec:
-
-        #lith = lithops.FunctionExecutor(runtime='lithops-ndvi-v312:01')
-        #lith.call_async(test, data=())
-        #res = lith.get_result()
-        #print(res)  # Prints 'hello'        
-        f = fexec.call_async(process_pdf, filetest)
-        print(f.result())
+    #lith = lithops.FunctionExecutor(runtime='lithops-ndvi-v312:01')
+    #lith.call_async(test, data=())
+    #res = lith.get_result()
+    #print(res)  # Prints 'hello'        
+    f = fexec.call_async(process_pdf, filetest)
+    print(f.result())
