441ecd86c24203c4ec95eb40644181c53156864e
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-05-26 22:02:08 +0930

    except

--- src/games/lithops_test_multipool.py
+++ src/games/lithops_test_multipool.py
@@ -92,7 +92,8 @@ if __name__ == "__main__":
         #async_result = pool.map_async(process_pdf, plist[0:3])
         async_result = pool.map_async(process_pdf, plist)
         try:
-            result = async_result.get()
+            #result = async_result.get()
+            result = async_result.get(throw_except=False)
             print(result)
         except TimeoutError:
             print("Timed out!")        
