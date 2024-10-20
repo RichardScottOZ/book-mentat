ee90e625250ef828b23e97084a3be880b65ac551
RichardScottOZ <rnmscott@netspace.net.au>
2024-10-20 12:03:02 +1030

    rigged

--- src/games/check_game_text.py
+++ src/games/check_game_text.py
@@ -7,8 +7,8 @@ input_path = "/mnt/usb_mount/books/Calibre Library"
 output_path = "/mnt/usb_mount/games/parseenumber"
 
 file = 'DC Heroes - Rigged Results - Bruce Humphrey.pdf_15225.pkl'
-file = 'D&D - d20 Pathfinder Village Ba - Ben Kent.pdf_22822.pkl'
-file = r"Mothership - What We Give To Al - Andrew D. O'Brien.pdf_26097.pkl"
+#file = 'D&D - d20 Pathfinder Village Ba - Ben Kent.pdf_22822.pkl'
+#file = r"Mothership - What We Give To Al - Andrew D. O'Brien.pdf_26097.pkl"
 
 with open(os.path.join(output_path,file),'rb') as f:
     elements = pickle.load(f)
